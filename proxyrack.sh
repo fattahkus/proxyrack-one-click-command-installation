39c505095ba77ad36a64613a1ea7e7a4292847db
Irfan Fattah <irfan.fattah.kusuma@student.uty.ac.id>
2023-08-19 12:00:05 +0800

    proxyrack-one-click-command-installation

--- proxyrack.sh
+++ proxyrack.sh
@@ -127,22 +127,22 @@ reg_device(){
   device_name="$dname"
 
   # register=`curl -X POST https://peer.proxyrack.com/api/device/add -H 'Api-Key: $token_id' -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"'"'device_id'"'":"'"'$device_id'"'","'"'device_name'"'":"'"'$device_name'"'"}' --compressed -D - -s -L`
-  register=`curl \
-    -X POST https://peer.proxyrack.com/api/device/add \
-    -H 'Api-Key: $token_id' \
-    -H 'Content-Type: application/json' \
-    -H 'Accept: application/json' \
-    -d '{"'"'device_id'"'":"'"'$device_id'"'","'"'device_name'"'":"'"'$device_name'"'"}' --compressed -D - -s -L`
-
-  response="$(echo "$register")"
-  echo "$response"
-
-  # echo "trying to register a device : curl \
+  # register=`curl \
   #   -X POST https://peer.proxyrack.com/api/device/add \
   #   -H 'Api-Key: $token_id' \
   #   -H 'Content-Type: application/json' \
   #   -H 'Accept: application/json' \
-  #   -d '{"'"'device_id'"'":"'"'$device_id'"'","'"'device_name'"'":"'"'$device_name'"'"}' --compressed -D - -s -L"
+  #   -d '{"'"'device_id'"'":"'"'$device_id'"'","'"'device_name'"'":"'"'$device_name'"'"}' --compressed -D - -s -L`
+
+  # response="$(echo "$register")"
+  # echo "$response"
+
+  echo "trying to register a device : curl \
+    -X POST https://peer.proxyrack.com/api/device/add \
+    -H 'Api-Key: $token_id' \
+    -H 'Content-Type: application/json' \
+    -H 'Accept: application/json' \
+    -d '{"'"'device_id'"'":"'"'$device_id'"'","'"'device_name'"'":"'"'$device_name'"'"}' --compressed -D - -s -L"
 }
 
 # 显示结果
